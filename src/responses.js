ca7b7ccc30ac8d1ec78306a09104f4f0d7dcd177
Matthew Roberts <mxr2091@rit.edu>
2021-02-24 20:11:09 -0500

    removed joke data, pointed the data to data/data.json

--- src/responses.js
+++ src/responses.js
@@ -1,15 +1,6 @@
-const jokesObj = [
-  { q: 'What do you call a very small valentine?', a: 'A valen-tiny!' },
-  { q: 'What did the dog say when he rubbed his tail on the sandpaper?', a: 'Ruff, Ruff!' },
-  { q: "Why don't sharks like to eat clowns?", a: 'Because they taste funny!' },
-  { q: 'What did the boy cat say to the girl cat?', a: "You're Purr-fect!" },
-  { q: "What is a frog's favorite outdoor sport?", a: 'Fly Fishing!' },
-  { q: 'I hate jokes about German sausages.', a: 'Theyre the wurst.' },
-  { q: 'Did you hear about the cheese factory that exploded in France?', a: 'There was nothing left but de Brie.' },
-  { q: 'Our wedding was so beautiful ', a: 'Even the cake was in tiers.' },
-  { q: 'Is this pool safe for diving?', a: 'It deep ends.' },
-  { q: 'Dad, can you put my shoes on?', a: 'I dont think theyll fit me.' },
-];
+const fs = require('fs');
+
+const data = fs.readFileSync(`${__dirname}/../data/data.json`);
 
 const respond = (request, response, content, type, length) => {
   response.writeHead(200, { 'Content-Type': type, 'Content-Length': length });
