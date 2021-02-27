266c9b5a66fc09d63ac30b8db5010e80b1af00de
Matthew Roberts <mxr2091@rit.edu>
2021-02-26 22:19:28 -0500

    external JSON file now parses correctly. basic responses working.

--- src/responses.js
+++ src/responses.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
+const helperHandler = require('./helper.js');
 
-const data = fs.readFileSync(`${__dirname}/../data/data.json`);
+const data = JSON.parse(fs.readFileSync(`${__dirname}/../data/data.json`));
 
 const respond = (request, response, content, type, length) => {
   response.writeHead(200, { 'Content-Type': type, 'Content-Length': length });
@@ -13,93 +14,78 @@ const respondHeaderOnly = (request, response, type, length) => {
   response.end();
 };
 
-// Source: https://stackoverflow.com/questions/2219526/how-many-bytes-in-a-javascript-string/29955838
-// Refactored to an arrow function by ACJ
-const getBinarySize = (string) => Buffer.byteLength(string, 'utf8');
-
-const getExercise = () => {
-  const number = Math.floor(Math.random() * jokesObj.length);
-  const responseObj = jokesObj[number];
-
-  return JSON.stringify(responseObj);
+// returns all workouts for a user, defaults to me :)
+const getAllWorkouts = (paramUser = "matthewroberts117") => {
+  const responseObj = () => { 
+    for (let i = 0; i <= data.length; i++) {
+      if (data[i].user === paramUser) {
+        return data[i];
+      }
+    }
+  };
+  return JSON.stringify(responseObj());
 };
 
-const getRandomJokes = (paramLimit = 1) => {
-  shuffleArray(jokesObj);
-
-  let limit = Number(paramLimit);
-  limit = !limit ? 1 : limit;
-  limit = limit < 1 ? 1 : limit;
-  limit = limit > jokesObj.length ? jokesObj.length : limit;
-
-  const responseObj = [];
-  for (let i = 0; i <= limit - 1; i++) {
-    responseObj[i] = jokesObj[i];
-  }
-
-  return JSON.stringify(responseObj);
-};
-
-const getRandomJokeResponse = (request, response, acceptedTypes, httpMethod) => {
+const getAllWorkoutsResponse = (request, response, params, acceptedTypes, httpMethod) => {
   if (acceptedTypes[0] === 'text/xml') {
-    // get the joke back in JSON form from a string
-    const joke = JSON.parse(getRandomJoke());
-    const responseXML = `
-     <jokes>
-      <joke>
-       <q>${joke.q}</q>
-       <a>${joke.a}</a>
-      </joke>
-     </jokes>
+    const workouts = JSON.parse(getAllWorkouts(params.user)).workouts;
+    let responseXML = `<workouts>`;
+
+    // write XML for each workout
+    for (let i = 0; i < Object.keys(workouts).length; i++) {
+      let exercises = workouts[i]["exercises"];
+      responseXML += 
+       `<workout>
+         <date>${workouts[i].date}</date>
+         <type>${workouts[i].type}</type>
+         <overallDifficulty>${workouts[i].overallDifficulty}</overallDifficulty>
+       `;
+
+      // write XML for each exercise
+      for (let j = 0; j < Object.keys(exercises).length; j++) {
+        let sets = exercises[j]["sets"];
+        responseXML += 
+         `<exercise>
+           <name>${exercises[j].name}</name>
+         `;
+
+        // write XML for each set
+        for (let k = 0; k < Object.keys(sets).length; k++) {
+          responseXML += 
+           `<set>
+             <reps>${sets[k].reps}</reps>
+             <weight>${sets[k].weight}</weight>
+             <setDifficulty>${sets[k].setDifficulty}</setDifficulty>
+            </set>
+           `;
+        }
+        responseXML += 
+         `</exercise>
+         `;
+      }
+      responseXML += 
+         `</workout>
+         `;
+    }
+    responseXML += 
+    `</workouts>
     `;
 
     if (httpMethod === 'HEAD') {
-      return respondHeaderOnly(request, response, 'text/xml', getBinarySize(responseXML));
+      return respondHeaderOnly(request, response, 'text/xml', helperHandler.getBinarySize(responseXML));
     }
-    return respond(request, response, responseXML, 'text/xml', getBinarySize(responseXML));
+    return respond(request, response, responseXML, 'text/xml', helperHandler.getBinarySize(responseXML));
   }
+
   if (httpMethod === 'HEAD') {
-    return respondHeaderOnly(request, response, 'application/json', getBinarySize(getRandomJoke()));
+    return respondHeaderOnly(request, response, 'application/json', helperHandler.getBinarySize(getAllWorkouts(params.user)));
   }
-  const jokeResponse = getRandomJoke();
-  return respond(request, response, jokeResponse, 'application/json', getBinarySize(jokeResponse));
-};
 
-const getRandomJokesResponse = (request, response, params, acceptedTypes, httpMethod) => {
-  if (acceptedTypes[0] === 'text/xml') {
-    const jokes = JSON.parse(getRandomJokes(params.limit));
-    let responseXML = `
-     <jokes>
-    `;
-    for (let i = 0; i < params.limit; i++) {
-      responseXML += ` 
-        <joke>
-         <q>${jokes[i].q}</q>
-         <a>${jokes[i].a}</a>
-        </joke>
-      `;
-    }
-    responseXML += `
-     </jokes>
-    `;
-    // console.log(responseXML);
-
-    if (httpMethod === 'HEAD') {
-      return respondHeaderOnly(request, response, 'text/xml', getBinarySize(responseXML));
-    }
-    return respond(request, response, responseXML, 'text/xml', getBinarySize(responseXML));
-  }
-  if (httpMethod === 'HEAD') {
-    return respondHeaderOnly(request, response, 'application/json', getBinarySize(getRandomJokes(params.limit)));
-  }
-  const jokeResponse = getRandomJokes(params.limit);
-  return respond(request, response, jokeResponse, 'application/json', getBinarySize(jokeResponse));
-};
-
-const getWorkoutRecordsResponse = (request, response, params, acceptedTypes, httpMethod) => {
-
+  const workoutResponse = getAllWorkouts(params.user);
+  // const workoutResponse = "test string";
+  return respond(request, response, workoutResponse, 'application/json', helperHandler.getBinarySize(workoutResponse));
 };
 
 module.exports = {
-  getWorkoutRecordsResponse
-}
+  getAllWorkoutsResponse
+};
