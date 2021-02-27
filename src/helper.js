8b6466e8bcd83f0bcb1fc7336106a33190af09ad
Matthew Roberts <mxr2091@rit.edu>
2021-02-26 22:18:01 -0500

    moved getBinarySize to here

---
+++ src/helper.js
@@ -0,0 +1,7 @@
+// Source: https://stackoverflow.com/questions/2219526/how-many-bytes-in-a-javascript-string/29955838
+// Refactored to an arrow function by ACJ
+const getBinarySize = (string) => Buffer.byteLength(string, 'utf8');
+
+module.exports = {
+  getBinarySize
+}
