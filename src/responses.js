9c438c4a99bd9c52b25beb8b68a1a9c37d6c93a8
Matthew Roberts <mxr2091@rit.edu>
2021-02-28 00:31:19 -0500

    small changes in formatting and cleanup

--- src/responses.js
+++ src/responses.js
@@ -16,8 +16,8 @@ const respondHeaderOnly = (request, response, type, length) => {
 };
 
 // returns all workouts for a user, defaults to me if no user param exists
-const getAllWorkouts = (paramUser = "matthewroberts117") => {
-  const responseObj = () => { 
+const getAllWorkouts = (paramUser = 'matthewroberts117') => {
+  const responseObj = () => {
     // loop through all objects and search for the user param
     for (let i = 0; i < data.length; i++) {
       if (data[i].user === paramUser) {
@@ -25,21 +25,21 @@ const getAllWorkouts = (paramUser = "matthewroberts117") => {
       }
     }
     // executes if the user param cannot be found
-    return "User " + paramUser + " does not exist.";
+    return `User ${paramUser} does not exist.`;
   };
   return JSON.stringify(responseObj());
 };
 
 const getAllWorkoutsResponse = (request, response, params, acceptedTypes, httpMethod) => {
   if (acceptedTypes[0] === 'text/xml') {
-    const workouts = JSON.parse(getAllWorkouts(params.user)).workouts;
-    let responseXML = `<workouts>`;
+    const { workouts } = JSON.parse(getAllWorkouts(params.user));
+    let responseXML = '<workouts>';
 
     // write XML for each workout
     for (let i = 0; i < Object.keys(workouts).length; i++) {
-      let exercises = workouts[i]["exercises"];
-      responseXML += 
-       `<workout>
+      const { exercises } = workouts[i];
+      responseXML
+       += `<workout>
          <date>${workouts[i].date}</date>
          <type>${workouts[i].type}</type>
          <overallDifficulty>${workouts[i].overallDifficulty}</overallDifficulty>
@@ -47,54 +47,52 @@ const getAllWorkoutsResponse = (request, response, params, acceptedTypes, httpMe
 
       // write XML for each exercise
       for (let j = 0; j < Object.keys(exercises).length; j++) {
-        let sets = exercises[j]["sets"];
-        responseXML += 
-         `<exercise>
+        const { sets } = exercises[j];
+        responseXML
+         += `<exercise>
            <name>${exercises[j].name}</name>
          `;
 
         // write XML for each set
         for (let k = 0; k < Object.keys(sets).length; k++) {
-          responseXML += 
-           `<set>
+          responseXML
+           += `<set>
              <reps>${sets[k].reps}</reps>
              <weight>${sets[k].weight}</weight>
              <setDifficulty>${sets[k].setDifficulty}</setDifficulty>
             </set>
            `;
         }
-        responseXML += 
-         `</exercise>
+        responseXML
+         += `</exercise>
          `;
       }
-      responseXML += 
-         `</workout>
+      responseXML
+         += `</workout>
          `;
     }
-    responseXML += 
-    `</workouts>
+    responseXML
+    += `</workouts>
     `;
 
     // XML HEAD request
     if (httpMethod === 'HEAD') {
       return respondHeaderOnly(request, response, 'text/xml', helperHandler.getBinarySize(responseXML));
     // XML GET request
-    } else {
-      return respond(request, response, responseXML, 'text/xml', helperHandler.getBinarySize(responseXML));
     }
+    return respond(request, response, responseXML, 'text/xml', helperHandler.getBinarySize(responseXML));
   }
 
   // JSON HEAD request
   if (httpMethod === 'HEAD') {
     return respondHeaderOnly(request, response, 'application/json', helperHandler.getBinarySize(getAllWorkouts(params.user)));
   // JSON GET request
-  } else {
-    const workoutResponse = getAllWorkouts(params.user);
-    // const workoutResponse = "test string";
-    return respond(request, response, workoutResponse, 'application/json', helperHandler.getBinarySize(workoutResponse));
   }
+  const workoutResponse = getAllWorkouts(params.user);
+  // const workoutResponse = "test string";
+  return respond(request, response, workoutResponse, 'application/json', helperHandler.getBinarySize(workoutResponse));
 };
 
 module.exports = {
-  getAllWorkoutsResponse
+  getAllWorkoutsResponse,
 };
